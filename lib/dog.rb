c771eaa967f8abe3422f1fdbeb51b152903641af
Marissa Zhong <mwz2101@columbia.edu>
2018-07-09 12:03:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,13 @@
 class Dog < ActiveRecord::Base
 
-  
+  def initialize(attributes)
+    attributes.each {|k,v| self.send(("#{k}="),v)}
+  end
+
+  def self.create(attributes)
+    new_dog = Dog.new
+
+    Dog.create(name: name)
+  end
 
 end
